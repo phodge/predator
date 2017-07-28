38645c8bd2429e2fec0f9ebca1c47203826d1d1b
Peter Hodge <peter.hodge84@gmail.com>
2017-07-29 08:41:26 +1000

    test: tests for grammar construction

---
+++ tests/test_010_grammar_construction.py
@@ -0,0 +1,36 @@
+"""
+The follow tests are designed to prove that you can _construct_ a tree of
+grammar items using the predator.grammar.Item subclasses.
+"""
+
+
+def test_construct_word():
+    assert False, 'Test is unfinished'
+
+
+def test_construct_literal():
+    assert False, 'Test is unfinished'
+
+
+def test_construct_regex():
+    assert False, 'Test is unfinished'
+
+
+def test_construct_linebreak():
+    assert False, 'Test is unfinished'
+
+
+def test_construct_choice():
+    assert False, 'Test is unfinished'
+
+
+def test_construct_sequence():
+    assert False, 'Test is unfinished'
+
+
+def test_construct_whitespace():
+    assert False, 'Test is unfinished'
+
+
+def test_construct_everything():
+    assert False, 'Test is unfinished'
