b0ee76bfdf1f5d33a15fbc02c3a1c7dedf4a7f56
Peter Hodge <peter.hodge84@gmail.com>
2017-07-29 11:52:24 +1000

    predator.common: new error class for grammar construction problems

--- predator/common.py
+++ predator/common.py
@@ -4,3 +4,10 @@ Errors and miscellaneous utilities.
 import re
 
 LINEBREAK_RE = re.compile(r'\r\n?|\n')
+
+
+class GrammarConstructionError(Exception):
+    """
+    Raised when something attempts to create an instance of a
+    predator.grammar.Item subclass using bad arguments.
+    """
