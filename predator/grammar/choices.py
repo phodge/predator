f43c87db668cb881e82d6c9e610bde37b633ef02
Peter Hodge <peter.hodge84@gmail.com>
2017-07-29 14:03:45 +1000

    predator/grammar: ability to construct Choice items

--- predator/grammar/choices.py
+++ predator/grammar/choices.py
@@ -6,3 +6,11 @@ class Choice(Item):
     A grammar object that attempts to match text using one of several other
     grammar Items.
     """
+    def __init__(self, name=None):
+        super().__init__(name)
+
+        self._choices = []
+
+    def addchoice(self, item):
+        assert isinstance(item, Item)
+        self._choices.append(item)
