0ba66526a41c9bcdc24f2212fa7c5721efd7c859
Peter Hodge <peter.hodge84@gmail.com>
2017-07-29 11:53:42 +1000

    tests: tests for Word items

--- tests/test_010_grammar_construction.py
+++ tests/test_010_grammar_construction.py
@@ -5,7 +5,44 @@ grammar items using the predator.grammar.Item subclasses.
 
 
 def test_construct_word():
-    assert False, 'Test is unfinished'
+    from predator.common import GrammarConstructionError
+    from predator.grammar import Word
+
+    # things that do work
+    Word('hello')
+    Word('a')
+    Word('AVeryLongWordThatWillAlsoWork')
+    Word('L3375p34k')
+
+    # things that won't work
+    things_that_fail = (
+        'multiple words',
+        'multiple\nlines',
+        # can't be wrapped in whitespace
+        ' hello',
+        'hello ',
+        # symbols aren't allowed
+        '!@#$',
+        # not even in the middle of a word
+        'he!!o',
+        # words can't start with a digit
+        '5hello',
+        # also can't be empty
+        '',
+        ' ',
+    )
+
+    for badword in things_that_fail:
+        try:
+            # symbols don't work
+            Word(badword)
+        except GrammarConstructionError:
+            pass
+        else:
+            raise Exception('Word({!r}) should have failed'.format(badword))
+
+    # make sure Word has a nice repr
+    _checkrepr(Word('hello'), ['hello'])
 
 
 def test_construct_literal():
@@ -34,3 +71,14 @@ def test_construct_whitespace():
 
 def test_construct_everything():
     assert False, 'Test is unfinished'
+
+
+def _checkrepr(item, lookfor):
+    """
+    Checks that a grammar item's repr() is sane.
+    """
+    r = repr(item)
+    assert r.startswith('<') and r.endswith('>')
+    assert item.__class__.__name__ in r
+    for text in lookfor:
+        assert text in r
